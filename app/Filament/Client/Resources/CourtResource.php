ffce690b330bbcd9293bdfb42f8bcbc28ddd4084
Luis Felipe <luis.belle@eloverde.com.br>
2024-10-23 18:05:08 -0300

    feature: Court resource client panel

---
+++ app/Filament/Client/Resources/CourtResource.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Client\Resources;
+
+use App\Filament\Client\Resources\CourtResource\Pages;
+use App\Filament\Client\Resources\CourtResource\RelationManagers;
+use App\Models\Court;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Infolists\Components\IconEntry;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
+use Filament\Resources\Resource;
+use Filament\Support\Enums\FontWeight;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CourtResource extends Resource
+{
+    protected static ?string $model = Court::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+        ->columns([
+            Tables\Columns\Layout\Stack::make([
+                Tables\Columns\Layout\Stack::make([
+                    Tables\Columns\TextColumn::make('name')
+                        ->weight(FontWeight::Bold),
+                    Tables\Columns\TextColumn::make('hour_value')
+                        ->formatStateUsing(fn (string $state): string => str($state)->after('://')->ltrim('www.')->trim('/'))
+                        ->color('gray')
+                        ->limit(30),
+                ]),
+            ])->space(3),
+            Tables\Columns\Layout\Panel::make([
+                Tables\Columns\Layout\Split::make([
+                    Tables\Columns\ColorColumn::make('color')
+                        ->grow(false),
+                    Tables\Columns\TextColumn::make('description')
+                        ->color('gray'),
+                ]),
+            ])->collapsible(),
+        ])
+        ->paginated(false)
+        ->filters([
+            //
+        ])
+        ->contentGrid([
+            'md' => 2,
+            'xl' => 3,
+        ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+      
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCourts::route('/'),
+            'create' => Pages\CreateCourt::route('/create'),
+            'edit' => Pages\EditCourt::route('/{record}/edit'),
+        ];
+    }
+}
