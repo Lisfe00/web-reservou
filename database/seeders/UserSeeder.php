5932526aa757283dc8fc88384fd8f6a33b569ada
Luis Felipe <luis.belle@eloverde.com.br>
2024-11-11 21:09:23 -0300

    fix: userSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,7 +16,6 @@ class UserSeeder extends Seeder
     {
         $user = User::create([
             'name' => 'Admin',
-            'social_name' => 'Analist User',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('password'),
         ]);
