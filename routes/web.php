c002ff43fc7d44caa13d977cf41e5521a6bb0094
Luis Felipe <luis.belle@eloverde.com.br>
2024-11-11 21:12:42 -0300

    feat: adicionar novas views e rotas para login, índice e visualização de quadras

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/court-view', function () {
+    return view('court-view');
+});
