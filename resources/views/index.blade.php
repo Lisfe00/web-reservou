940f9957a91582cd1bf4ff8bc244bd80283601b1
Esthefani-Possamai <esthefani_possamai@unesc.net>
2024-11-21 20:04:47 -0300

    feat: implementação da tela inicial

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,7 @@
     <title>Reservou</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{env('APP_URL')}}/css/style.css">
+    <link rel="icon" href="{{env('APP_URL')}}/images/icone.svg" type="image/x-icon">
 </head>
 
 <body class="body-style">
@@ -19,8 +20,8 @@
                     <option value="1">Cocal do Sul</option>
                     <option value="2">Criciúma</option>
                 </select>
-                <a href="#">Quadras</a>
-                <a href="#">Minhas reservas</a>
+                <a href="#" class="link">Quadras</a>
+                <a href="#" class="link">Minhas reservas</a>
                 <div>Sair</div>
             </div>
         </header>
@@ -48,17 +49,111 @@
                         </div>
                     </div>
                 </section>
-                <img width="500" height="800" src="{{env('APP_URL')}}/images/inicio.svg" alt="tag">
+                <img width="800" src="{{env('APP_URL')}}/images/inicio.svg" alt="tag" class="fullscreen-image">
+                <!--  height="800" -->
             </header>
         </main>
     </article>
     <article class="list">
-        <div class="item-list"></div>
-        <div class="item-list"></div>
-        <div class="item-list"></div>
+        <div class="item-list">
+            <div class="card-image-container">
+                <img src="{{env('APP_URL')}}/images/quadraTeste.jpeg" alt="quadra-teste" class="card-image">
+            </div>
+            <div class="info">
+                <span>Quadra de futebol</span>
+                <div class="ver-mais">
+                    <button type="button" class="button">Ver mais</button>
+                    <icon>❤️</icon>
+                </div>
+            </div>
+        </div>
+        <div class="item-list">
+            <div class="card-image-container">
+                <img src="{{env('APP_URL')}}/images/quadraTeste.jpeg" alt="quadra-teste" class="card-image">
+            </div>
+            <div class="info">
+                <span>Quadra de futebol</span>
+                <div class="ver-mais">
+                    <button type="button" class="button">Ver mais</button>
+                    <icon>❤️</icon>
+                </div>
+            </div>
+        </div>
+        <div class="item-list">
+            <div class="card-image-container">
+                <img src="{{env('APP_URL')}}/images/quadraTeste.jpeg" alt="quadra-teste" class="card-image">
+            </div>
+            <div class="info">
+                <span>Quadra de futebol</span>
+                <div class="ver-mais">
+                    <button type="button" class="button">Ver mais</button>
+                    <icon>❤️</icon>
+                </div>
+            </div>
+        </div>
+        <div class="item-list">
+            <div class="card-image-container">
+                <img src="{{env('APP_URL')}}/images/quadraTeste.jpeg" alt="quadra-teste" class="card-image">
+            </div>
+            <div class="info">
+                <span>Quadra de futebol</span>
+                <div class="ver-mais">
+                    <button type="button" class="button">Ver mais</button>
+                    <icon>❤️</icon>
+                </div>
+            </div>
+        </div>
+        <div class="item-list">
+            <div class="card-image-container">
+                <img src="{{env('APP_URL')}}/images/quadraTeste.jpeg" alt="quadra-teste" class="card-image">
+            </div>
+            <div class="info">
+                <span>Quadra de futebol</span>
+                <div class="ver-mais">
+                    <button type="button" class="button">Ver mais</button>
+                    <icon>❤️</icon>
+                </div>
+            </div>
+        </div>
+        <div class="item-list">
+            <div class="card-image-container">
+                <img src="{{env('APP_URL')}}/images/quadraTeste.jpeg" alt="quadra-teste" class="card-image">
+            </div>
+            <div class="info">
+                <span>Quadra de futebol</span>
+                <div class="ver-mais">
+                    <button type="button" class="button">Ver mais</button>
+                    <icon>❤️</icon>
+                </div>
+            </div>
+        </div>
+        <div class="item-list">
+            <div class="card-image-container">
+                <img src="{{env('APP_URL')}}/images/quadraTeste.jpeg" alt="quadra-teste" class="card-image">
+            </div>
+            <div class="info">
+                <span>Quadra de futebol</span>
+                <div class="ver-mais">
+                    <button type="button" class="button">Ver mais</button>
+                    <icon>❤️</icon>
+                </div>
+            </div>
+        </div>
+        <div class="item-list">
+            <div class="card-image-container">
+                <img src="{{env('APP_URL')}}/images/quadraTeste.jpeg" alt="quadra-teste" class="card-image">
+            </div>
+            <div class="info">
+                <span>Quadra de futebol</span>
+                <div class="ver-mais">
+                    <button type="button" class="button">Ver mais</button>
+                    <icon>❤️</icon>
+                </div>
+            </div>
+        </div>
     </article>
 
-    <footer></footer>
+    <footer class="footer"></footer>
 </body>
 
 </html>
